54ca40035ffb1b782b63017131c9f1ff8b3e3894
Vaibhav Pujari <276vaibhav+github@gmail.com>
2024-04-03 12:59:58 +0530

    accounts - API documentation

--- accounts/src/main/java/com/demo/bank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/demo/bank/accounts/controller/AccountsController.java
@@ -17,9 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.demo.bank.accounts.constants.SuccessMessages;
 import com.demo.bank.accounts.dto.AccountDto;
 import com.demo.bank.accounts.dto.CustomerDto;
+import com.demo.bank.accounts.dto.ErrorResponseDto;
 import com.demo.bank.accounts.dto.ResponseDto;
 import com.demo.bank.accounts.service.IAccountService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -28,10 +35,39 @@ import lombok.AllArgsConstructor;
 @RequestMapping(path = "/api/v1", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 @Validated
+@Tag(
+        name = "CRUD REST APIs for Accounts in DemoBank",
+        description = "CRUD REST APIs in DemoBank to CREATE, UPDATE, FETCH AND DELETE account details"
+)
 public class AccountsController {
 
     IAccountService iAccountService;
 
+    @Operation(
+        summary = "Create Account REST API",
+        description = "REST API to create new Customer & Account inside DemoBank"
+    )
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "201",
+            description = "201 CREATED"
+        ),
+        @ApiResponse(
+            responseCode = "400",
+            description = "400 BAD REQUEST",
+            content = @Content(
+                schema = @Schema(implementation = ErrorResponseDto.class)
+            )
+        ),
+        @ApiResponse(
+            responseCode = "500",
+            description = "500 INTERNAL SERVER ERROR",
+            content = @Content(
+                schema = @Schema(implementation = ErrorResponseDto.class)
+            )
+        )
+    }
+    )
     @PostMapping("/accounts")
     public ResponseEntity<ResponseDto> createAccount(
         @Valid @RequestBody CustomerDto customerDto
@@ -45,6 +81,24 @@ public class AccountsController {
                                   SuccessMessages.ACCOUNT_CREATED));
     }
 
+    @Operation(
+        summary = "Fetch Account Details REST API",
+        description = "REST API to fetch Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            description = "200 OK"
+        ),
+        @ApiResponse(
+            responseCode = "500",
+            description = "500 INTERNAL SERVER ERROR",
+            content = @Content(
+                schema = @Schema(implementation = ErrorResponseDto.class)
+            )
+        )
+    }
+    )
     @GetMapping("/accounts")
     public ResponseEntity<AccountDto> getAccountByCustomerMobileNumber(
         @RequestParam
@@ -57,6 +111,38 @@ public class AccountsController {
             .body(accountDto);
     }
 
+    @Operation(
+        summary = "Update Account Details REST API",
+        description = "REST API to update Customer & Account details based on a account number"
+    )
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            description = "200 OK"
+        ),
+        @ApiResponse(
+            responseCode = "404",
+            description = "404 NOT FOUND",
+            content = @Content(
+                schema = @Schema(implementation = ErrorResponseDto.class)
+            )
+        ),
+        @ApiResponse(
+            responseCode = "400",
+            description = "400 BAD REQUEST",
+            content = @Content(
+                schema = @Schema(implementation = ErrorResponseDto.class)
+            )
+        ),
+        @ApiResponse(
+            responseCode = "500",
+            description = "500 INTERNAL SERVER ERROR",
+            content = @Content(
+                schema = @Schema(implementation = ErrorResponseDto.class)
+            )
+        )
+    }
+    )
     @PutMapping("/accounts/{accountNumber}")
     public ResponseEntity<AccountDto> updateAccount(
         @PathVariable(name = "accountNumber") Long accountNumber,
@@ -68,6 +154,21 @@ public class AccountsController {
             .body(accountDtoNew);
     }
 
+    @Operation(
+        summary = "Delete Account & Customer Details REST API",
+        description = "REST API to delete Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+        @ApiResponse(
+            responseCode = "200",
+            description = "200 OK"
+        ),
+        @ApiResponse(
+            responseCode = "417",
+            description = "417 EXPECTATION FAILED"
+        )
+    }
+    )
     @DeleteMapping("/accounts")
     public ResponseEntity<ResponseDto> deleteAccountByCustomerMobileNumber(
         @RequestParam
@@ -81,9 +182,9 @@ public class AccountsController {
                 .body(new ResponseDto(HttpStatus.OK.toString(), SuccessMessages.ACCOUNT_DELETED));
         } else {
             return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.toString(),
-                    HttpStatus.INTERNAL_SERVER_ERROR.toString()));
+                .status(HttpStatus.EXPECTATION_FAILED)
+                .body(new ResponseDto(HttpStatus.EXPECTATION_FAILED.toString(),
+                    HttpStatus.EXPECTATION_FAILED.toString()));
         }
     }
 }
--- accounts/src/main/java/com/demo/bank/accounts/dto/AccountDto.java
+++ accounts/src/main/java/com/demo/bank/accounts/dto/AccountDto.java
@@ -1,19 +1,36 @@
 package com.demo.bank.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+    name = "Account",
+    description = "Schema to hold Account and Customer information"
+)
 public class AccountDto {
     @NotEmpty(message = "Account number cannot be null or empty")
     @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    @Schema(
+        description = "Account number",
+        example = "1234567890"
+    )
     private String accountNumber;
 
     @NotEmpty(message = "Account type cannot be null or empty")
+    @Schema(
+        description = "Account type",
+        example = "Savings"
+    )
     private String accountType;
 
     @NotEmpty(message = "Branch address cannot be null or empty")
+    @Schema(
+        description = "Address of the branch",
+        example = "123 Dummy Street, Dummy Town"
+    )
     private String branchAddress;
     
     CustomerDto customerDto;
