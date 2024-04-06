a1c31d875fa3269aec38a8459d797d6f03339642
Vaibhav Pujari <276vaibhav+github@gmail.com>
2024-04-06 13:25:08 +0530

    actuator - refresh

--- accounts/src/main/java/com/demo/bank/accounts/dto/ContactInfoDto.java
+++ accounts/src/main/java/com/demo/bank/accounts/dto/ContactInfoDto.java
@@ -4,11 +4,15 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
 @ConfigurationProperties(prefix = "accounts")
-public record ContactInfoDto(
-    String message,
-    Map<String, String> contactDetails, 
-    List<String> onCallSupport) {
+@Getter @Setter
+public class ContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 }
