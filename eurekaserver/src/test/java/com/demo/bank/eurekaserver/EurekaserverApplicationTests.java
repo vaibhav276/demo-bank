9b763501ce381ed9d1a7958a4fb761f5d842f5a4
Vaibhav Pujari <276vaibhav+github@gmail.com>
2024-04-08 14:56:56 +0530

    Eureka server

---
+++ eurekaserver/src/test/java/com/demo/bank/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.bank.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
