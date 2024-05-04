62fc851cafc4f6ecc9889dcaea46f09ea23f36a1
Vaibhav Pujari <276vaibhav+github@gmail.com>
2024-05-04 15:27:34 +0530

    RabbitMQ pubsub: docker-compose

--- e2e-test/e2e_test.sh
+++ e2e-test/e2e_test.sh
@@ -21,57 +21,57 @@ cat get_account.json | jq .
 
 ACCOUNT_NUMBER=`cat get_account.json | jq -r .accountNumber`
 
-# echo "Updating account ${ACCOUNT_NUMBER}..."
-# http PUT ${GATEWAY_SERVER}/demobank/accounts/api/v1/accounts/$ACCOUNT_NUMBER accountNumber=${ACCOUNT_NUMBER} accountType="Savings" branchAddress="124 Dummy Street, Dummy Town" customerDto:='{"name": "Peter Pan", "email":"peter@pan.com"}' -A bearer -a $ACCESS_TOKEN
-#  > get_account1.json
-# 
-# echo "Account after updating"
-# cat get_account1.json | jq .
-# 
-# echo "*****"
-# 
-# echo "Creating loan for customer with mobile number ${MOBILE_NUMBER}..."
-# http POST ${GATEWAY_SERVER}/demobank/loans/api/v1/loans mobileNumber==${MOBILE_NUMBER} -A bearer -a $ACCESS_TOKEN
-# 
-# 
-# echo "Getting details of loan with mobile number ${MOBILE_NUMBER}..."
-# http ${GATEWAY_SERVER}/demobank/loans/api/v1/loans mobileNumber==${MOBILE_NUMBER} > get_loan.json
-# cat get_loan.json | jq .
-# 
-# LOAN_NUMBER=`cat get_loan.json | jq -r .loanNumber`
-# 
-# echo "Updating loan ${LOAN_NUMBER} for customer with mobile number ${MOBILE_NUMBER}..."
-# http PUT ${GATEWAY_SERVER}/demobank/loans/api/v1/loans mobileNumber=${MOBILE_NUMBER} loanNumber=${LOAN_NUMBER} loanType="Home Loan" totalLoan:=1272 amountPaid:=233 outstandingAmount:=348 -A bearer -a $ACCESS_TOKEN
-# 
-# 
-# echo "Getting details of loan with mobile number ${MOBILE_NUMBER}..."
-# http ${GATEWAY_SERVER}/demobank/loans/api/v1/loans mobileNumber==${MOBILE_NUMBER} > get_loan1.json
-# 
-# echo "Loan after updating"
-# cat get_loan1.json | jq .
-# 
-# echo "*****"
-# 
-# echo "Creating card for customer with mobile number ${MOBILE_NUMBER}..."
-# http POST ${GATEWAY_SERVER}/demobank/cards/api/v1/cards mobileNumber==${MOBILE_NUMBER} -A bearer -a $ACCESS_TOKEN
-# 
-# 
-# echo "Getting details of card with mobile number ${MOBILE_NUMBER}..."
-# http ${GATEWAY_SERVER}/demobank/cards/api/v1/cards mobileNumber==${MOBILE_NUMBER} > get_card.json
-# cat get_card.json | jq .
-# 
-# CARD_NUMBER=`cat get_card.json | jq -r .cardNumber`
-# 
-# echo "Updating card ${CARD_NUMBER} for customer with mobile number ${MOBILE_NUMBER}..."
-# http PUT ${GATEWAY_SERVER}/demobank/cards/api/v1/cards mobileNumber=${MOBILE_NUMBER} cardNumber=${CARD_NUMBER} cardType="Credit Card" totalLimit:=200000 amountUsed:=20454 availableAmount:=23499 -A bearer -a $ACCESS_TOKEN
-# 
-# 
-# echo "Getting details of card with mobile number ${MOBILE_NUMBER}..."
-# http ${GATEWAY_SERVER}/demobank/cards/api/v1/cards mobileNumber==${MOBILE_NUMBER} > get_card1.json
-# cat get_card1.json | jq .
-# 
-# echo "*****"
-# 
-# echo "Getting all customer details for customer with mobile number ${MOBILE_NUMBER}..."
-# http ${GATEWAY_SERVER}/demobank/accounts/api/v1/customers mobileNumber==${MOBILE_NUMBER} > get_all.json
-# cat get_all.json | jq .
+echo "Updating account ${ACCOUNT_NUMBER}..."
+http PUT ${GATEWAY_SERVER}/demobank/accounts/api/v1/accounts/$ACCOUNT_NUMBER accountNumber=${ACCOUNT_NUMBER} accountType="Savings" branchAddress="124 Dummy Street, Dummy Town" customerDto:='{"name": "Peter Pan", "email":"peter@pan.com"}' -A bearer -a $ACCESS_TOKEN
+ > get_account1.json
+
+echo "Account after updating"
+cat get_account1.json | jq .
+
+echo "*****"
+
+echo "Creating loan for customer with mobile number ${MOBILE_NUMBER}..."
+http POST ${GATEWAY_SERVER}/demobank/loans/api/v1/loans mobileNumber==${MOBILE_NUMBER} -A bearer -a $ACCESS_TOKEN
+
+
+echo "Getting details of loan with mobile number ${MOBILE_NUMBER}..."
+http ${GATEWAY_SERVER}/demobank/loans/api/v1/loans mobileNumber==${MOBILE_NUMBER} > get_loan.json
+cat get_loan.json | jq .
+
+LOAN_NUMBER=`cat get_loan.json | jq -r .loanNumber`
+
+echo "Updating loan ${LOAN_NUMBER} for customer with mobile number ${MOBILE_NUMBER}..."
+http PUT ${GATEWAY_SERVER}/demobank/loans/api/v1/loans mobileNumber=${MOBILE_NUMBER} loanNumber=${LOAN_NUMBER} loanType="Home Loan" totalLoan:=1272 amountPaid:=233 outstandingAmount:=348 -A bearer -a $ACCESS_TOKEN
+
+
+echo "Getting details of loan with mobile number ${MOBILE_NUMBER}..."
+http ${GATEWAY_SERVER}/demobank/loans/api/v1/loans mobileNumber==${MOBILE_NUMBER} > get_loan1.json
+
+echo "Loan after updating"
+cat get_loan1.json | jq .
+
+echo "*****"
+
+echo "Creating card for customer with mobile number ${MOBILE_NUMBER}..."
+http POST ${GATEWAY_SERVER}/demobank/cards/api/v1/cards mobileNumber==${MOBILE_NUMBER} -A bearer -a $ACCESS_TOKEN
+
+
+echo "Getting details of card with mobile number ${MOBILE_NUMBER}..."
+http ${GATEWAY_SERVER}/demobank/cards/api/v1/cards mobileNumber==${MOBILE_NUMBER} > get_card.json
+cat get_card.json | jq .
+
+CARD_NUMBER=`cat get_card.json | jq -r .cardNumber`
+
+echo "Updating card ${CARD_NUMBER} for customer with mobile number ${MOBILE_NUMBER}..."
+http PUT ${GATEWAY_SERVER}/demobank/cards/api/v1/cards mobileNumber=${MOBILE_NUMBER} cardNumber=${CARD_NUMBER} cardType="Credit Card" totalLimit:=200000 amountUsed:=20454 availableAmount:=23499 -A bearer -a $ACCESS_TOKEN
+
+
+echo "Getting details of card with mobile number ${MOBILE_NUMBER}..."
+http ${GATEWAY_SERVER}/demobank/cards/api/v1/cards mobileNumber==${MOBILE_NUMBER} > get_card1.json
+cat get_card1.json | jq .
+
+echo "*****"
+
+echo "Getting all customer details for customer with mobile number ${MOBILE_NUMBER}..."
+http ${GATEWAY_SERVER}/demobank/accounts/api/v1/customers mobileNumber==${MOBILE_NUMBER} > get_all.json
+cat get_all.json | jq .
